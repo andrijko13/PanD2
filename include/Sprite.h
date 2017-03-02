2f4cee61627aa2f3b0b7852d02c2ef413334a458
Andriy Suden <andriykosud@gmail.com>
2017-03-01 19:53:49 -0600

    Added position class and updated header file for sprite

--- include/Sprite.h
+++ include/Sprite.h
@@ -4,11 +4,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-class AJSprite {
+/*
+ * This class is used to create generic sprites. These should conform to typical Sprite protocol,
+ * and must therefore implement features such as collision detection, impulse generation, and
+ * basic kinematic properties for rigid bodies with circular physics bodies.
+ *
+ * 
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+*/
+
+class AJPosition {
     public:
+        double const getX() { return this->x; };
+        double const getY() { return this->y; };
+        void const setX(const double &xpos) { this->x = xpos; };
+        void const setY(const double &ypos) { this->y = ypos; };
+
+    private:
+        double x;
+        double y;
+};
+
+class AJSprite {
+
+    public:
+
+
+        // METHODS
         AJSprite();
         ~AJSprite();
+
+
+        // iVARS
+
     private:
+        AJPosition position;
         int radius;
+        
 };
 #endif
